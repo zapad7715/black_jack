ecb2b94b1ff9eeb77f047346962be0372af81b29
zapad7715 <zapad7715@ya.ru>
2017-07-06 12:52:58 +0300

    Hand

---
+++ hand.rb
@@ -0,0 +1,16 @@
+class Hand
+
+  attr_reader :cards
+
+  @cards = []
+
+  def take_card(deck)
+    @cards << deck.pop
+  end
+
+  def score
+    score = 0
+    @cards.each { |card| score += card.value }
+    score
+  end
+end
