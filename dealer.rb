41dbde5d5dc58a9506f5f3eed2f7690c236090cd
zapad7715 <zapad7715@ya.ru>
2017-07-06 23:12:32 +0300

    Dealer hand

--- dealer.rb
+++ dealer.rb
@@ -1,13 +1,21 @@
 require_relative 'bank'
+require_relative 'hand'
 
 # frozen_string_literal: true
 
-class Dealer < Hand
+class Dealer
   include Bank
 
   attr_reader :hand
 
   def initialize
     @hand = Hand.new
+    class << @hand
+      alias real_take_card take_card
+      def take_card(deck)
+        raise 'Dealer pass.' if score >= 19
+        real_take_card(deck)
+      end
+    end
   end
 end
