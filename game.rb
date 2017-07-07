621264d3ccc00c6d31826f5ecf46a0551cc2fd5a
zapad7715 <zapad7715@ya.ru>
2017-07-07 19:30:19 +0300

    Game #distribution #reset_deck

--- game.rb
+++ game.rb
@@ -1,7 +1,19 @@
 # frozen_string_literal: true
 
 class Game
-  def initialize(deck)
-    @deck = deck
+  def initialize(player)
+    @dealer = Dealer.new
+    @player = player
+    @deck = Deck.new
+    destribution(@deck)
+  end
+
+  def reset_deck
+    @deck = Deck.new
+  end
+
+  def distribution(deck)
+    2.times { @player.hand.take_card(deck) }
+    2.times { @dealer.hand.take_card(deck) }
   end
 end
